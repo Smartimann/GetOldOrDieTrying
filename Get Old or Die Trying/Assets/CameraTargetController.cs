c183c8589a79cfeaab8adfcae90a014ba012bbfa
Moritz <moe-stoll@web.de>
2018-04-10 14:51:04 +0200

    Pathfinding + Basic Movement

---
+++ Get Old or Die Trying/Assets/CameraTargetController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTargetController : MonoBehaviour {
+    public GameObject player;
+    public float height = 35f;
+    public float centerValue = 20f;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = player.transform.position;
+        pos.x = pos.x - centerValue;
+        pos.z = pos.z - centerValue;
+        pos.y = height;
+        transform.position = pos;
+        
+	}
+}
