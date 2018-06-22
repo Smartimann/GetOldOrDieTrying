a0451b1f971f5f1058a9332e9667b352da154a5c
Moritz <moe-stoll@web.de>
2018-06-22 18:04:40 +0200

    New Level

---
+++ Get Old or Die Trying/Assets/GateBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateBehaviour : MonoBehaviour {
+    public GameObject Player;
+    bool inXRange;
+    bool inYRange;
+    // Use this for initialization
+    float width = 5f;
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        inXRange = Player.transform.position.x > this.transform.position.x - width && Player.transform.position.x < this.transform.position.x + width;
+        inYRange = Player.transform.position.y > this.transform.position.y - width && Player.transform.position.y < this.transform.position.y + width;
+        if (inXRange && inYRange)
+        {
+            Player.GetComponent<UnityEngine.AI.NavMeshAgent>().Warp(new Vector3(-475.0595f, -165.8549f, 260.9867f));
+            //Player.transform.position = new Vector3(-500f, -168.5f, 263f);
+            Debug.Log("Entered");
+        }
+    }
+}
---
+++ Get Old or Die Trying/Assets/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        AudioSource audio = GetComponent<AudioSource>();
+        wind.Play();
+        wind.Play(44100);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
